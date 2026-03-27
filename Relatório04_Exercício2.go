b3f54cc7fe0032c9ba4c5c46a279b5d6b9ed9407
deboranegri <deboranegri05@hotmail.com>
2026-03-27 18:28:29 -0300

    Create Relatório04_Exercício2.go

---
+++ Relatório04_Exercício2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+	fmt.Print("Digite os três lados do triângulo: ")
+	fmt.Scan(&a, &b, &c)
+
+	// Verifica se forma um triângulo válido
+	if a+b > c && a+c > b && b+c > a {
+		switch {
+		case a == b && b == c:
+			fmt.Println("Triângulo Equilátero")
+		case a == b || a == c || b == c:
+			fmt.Println("Triângulo Isósceles")
+		default:
+			fmt.Println("Triângulo Escaleno")
+		}
+	} else {
+		fmt.Println("Erro: os valores não formam um triângulo válido.")
+	}
+}
