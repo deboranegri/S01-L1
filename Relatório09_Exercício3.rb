1727d8cb3591c6d0f2545d21195e5c456d31b804
deboranegri <deboranegri05@hotmail.com>
2026-04-30 19:05:30 -0300

    Create Relatório09_Exercício3.rb

---
+++ Relatório09_Exercício3.rb
@@ -0,0 +1,43 @@
+class MicroondasTelefone
+  def initialize(nome)
+    @nome = nome
+  end
+
+  def ativar
+    puts "Banana Verde detectada! O Microondas-Telefone (#{@nome}) está operando!"
+  end
+end
+
+class Relatorio
+  attr_reader :nome_projeto, :autor
+
+  def initialize(nome_projeto, autor)
+    @nome_projeto = nome_projeto
+    @autor = autor
+  end
+end
+
+class LabRegistro
+  def initialize
+    @microondas = MicroondasTelefone.new("nome provisório")
+    @registros = []
+  end
+
+  def adicionar_registro(nome_projeto, autor)
+    @microondas.ativar
+    @registros << Relatorio.new(nome_projeto, autor)
+  end
+
+  def listar_experimentos
+    puts "\n Experimentos registrados:"
+    @registros.each do |r|
+      puts "  Projeto: #{r.nome_projeto} | Autor: #{r.autor}"
+    end
+  end
+end
+
+lab = LabRegistro.new
+lab.adicionar_registro("KRR", "Okabe Rintaro")
+lab.adicionar_registro("CERN Hack", "Makise Kurisu")
+lab.adicionar_registro("Divergência 1%", "Hashida Itaru")
+lab.listar_experimentos
